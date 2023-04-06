1433378bab92aec0b8fd21908d12f464bb4eb63b
Wen Yang <wenyang.linux@foxmail.com>
2023-04-07 07:54:54 +0800

    glibc: install as the standard system library is ‘--prefix=/usr’ for GNU/Linux systems

--- packages/glibc/glibc.mk
+++ packages/glibc/glibc.mk
@@ -9,7 +9,7 @@ define glibc/build :=
 	+cd $(glibc/dir)
 	$(info glibc/dir: $(glibc/dir))
 	+mkdir -p build && cd build
-	+$(CROSS_MAKE_ENV) ../configure aarch64-none-linux-gnu --host=aarch64-none-linux-gnu CFLAGS="-O2 " --build=x86_64-pc-linux-gnu --prefix=  --enable-add-ons
+	+$(CROSS_MAKE_ENV) ../configure aarch64-none-linux-gnu --host=aarch64-none-linux-gnu CFLAGS="-O2 " --build=x86_64-pc-linux-gnu --prefix=/usr  --enable-add-ons
 	+$(CROSS_MAKE_ENV) '$(MAKE)' -j 8
 endef
 
