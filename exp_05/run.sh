758721800fb65b30263bcec3e522e12e0319165c
Wen Yang <wenyang.linux@foxmail.com>
2023-04-07 07:58:42 +0800

    exp_05: add gdb for debugging

---
+++ exp_05/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+BASE=`pwd`
+QEMU=${BASE}/../tools/qemu
+OUTPUT=${BASE}/work/output
+
+${QEMU}/qemu-system-aarch64 \
+	-L ${QEMU}/pc-bios/  \
+	-m 1G -smp 4 -M virt -nographic -cpu cortex-a57 \
+	-kernel ${OUTPUT}/Image.gz \
+	-initrd ${OUTPUT}/rootfs.gz \
+	-append "console=ttyAMA0 rdinit=/sbin/init " 
