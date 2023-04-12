f502dcd4bd21912a35206e777d7abd0854394fc2
Wen Yang <wenyang.linux@foxmail.com>
2023-04-13 04:27:53 +0800

    linux: introducing perf tools

--- packages/linux/linux.mk
+++ packages/linux/linux.mk
@@ -11,25 +11,23 @@ endef
 
 define linux/build :=
 	+cd $(linux/dir)
-	+mkdir -p ../build
-	+$(MAKE) ARCH=$(ARCH) $(KCFG)  O=../build
-	+cd ../build
+	+$(MAKE) ARCH=$(ARCH) $(KCFG)
 	if [ $(INITRD) -eq  1 ]; then
 		sed -i 's/# CONFIG_BLK_DEV_RAM is not set/CONFIG_BLK_DEV_RAM=y/' .config
 		sed -i '/CONFIG_BLK_DEV_RAM=y/a\CONFIG_BLK_DEV_RAM_COUNT=16'  .config
 		sed -i '/CONFIG_BLK_DEV_RAM=y/a\CONFIG_BLK_DEV_RAM_SIZE=4096' .config
 	fi
 
-	+$(CROSS_ENV_RAW) $(MAKE) ARCH=$(ARCH) CROSS_COMPILE=$(CROSS_NAME)- $(IMG) modules dtbs tools/perf -j 8
+	+$(CROSS_ENV_RAW) $(MAKE) ARCH=$(ARCH) CROSS_COMPILE=$(CROSS_NAME)- $(IMG) modules dtbs -j 8
+	+cd tools && $(CROSS_ENV_RAW) $(MAKE) ARCH=$(ARCH) CROSS_COMPILE=$(CROSS_NAME)- perf -j 8
 endef
 
 define linux/install :=
-	+cd $(linux/dir)/../build
+	+cd $(linux/dir)/
 	+cp arch/$(ARCH)/boot/$(IMG) $(HOST)/
 	+cp arch/$(ARCH)/boot/dts/$(DTB) $(HOST)/
 	+$(CROSS_ENV_RAW) make ARCH=$(ARCH) CROSS_COMPILE=$(CROSS_NAME)- INSTALL_MOD_PATH=$(HOST)/sysroot/usr INSTALL_MOD_STRIP=1 modules_install
 	+$(CROSS_ENV_RAW) make ARCH=$(ARCH) CROSS_COMPILE=$(CROSS_NAME)- INSTALL_HDR_PATH=$(HOST)/sysroot/usr headers_install
-#	+cd $(linux/dir)/../build/source/tools
-#	+$(CROSS_ENV_RAW) make ARCH=$(ARCH) CROSS_COMPILE=$(CROSS_NAME)- prefix=$(HOST)/sysroot/usr/ perf_install
+	+cd tools && $(CROSS_ENV_RAW) make ARCH=$(ARCH) CROSS_COMPILE=$(CROSS_NAME)- prefix=$(HOST)/sysroot/usr/ perf_install
 endef
 
