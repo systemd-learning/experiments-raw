32c8be1c9d846d24e171a3f6329569a3207dfef4
Wen Yang <wenyang.linux@foxmail.com>
2023-04-26 22:46:40 -0700

    demo_07: using systemd in diskimg
    alse add the bash package for further use

---
+++ packages/bash/bash.mk
@@ -0,0 +1,16 @@
+include $(BASE)/../common/env.mk
+
+bash/VERSION := 5.2
+bash/TARBALL := https://ftp.gnu.org/gnu/bash/bash-$(bash/VERSION).tar.gz
+bash/dir = $(BUILD)/bash/bash-$(bash/VERSION)_build.$(LOCAL_BUILD)
+
+define bash/build :=
+	+cd $(bash/dir)
+	+$(CROSS_MAKE_ENV) ./configure aarch64-none-linux-gnu --build=x86_64-pc-linux-gnu --host=aarch64-none-linux-gnu --target=aarch64-none-linux-gnu
+	+$(CROSS_MAKE_ENV) '$(MAKE)' -j 8
+endef
+
+define bash/install :=
+	+cd $(bash/dir)
+	+'$(MAKE)' install DESTDIR=$(HOST)/sysroot/
+endef
